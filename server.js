8ed6cedf5a6702f060351bd80b2cbb2e5775eb01
Bartosz <WonderfulLifeVision@gmail.com>
2021-03-16 19:37:23 +0100

    commit to remove

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 
+const path = require('path');
+
 const app = express();// Connect Database
 connectDB();
 
